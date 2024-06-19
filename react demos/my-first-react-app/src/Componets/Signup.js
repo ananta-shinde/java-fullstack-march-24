9a9c6c512dba84631217d8acc1265e0c9decf0c4
aShinde <ananta.shinde@honeywellcom>
2024-06-19 12:55:15 +0530

    19/06/24

--- react demos/my-first-react-app/src/Componets/Signup.js
+++ react demos/my-first-react-app/src/Componets/Signup.js
@@ -1,30 +1,56 @@
 import { Link } from "react-router-dom";
 
 const Signup = () => {
+    var name = "";
+    var email = ""
+    var password ="";
+    var confirmPassword ="";
+    
+    function handleChange(event){
+        if(event.target.name == "name"){
+            name = event.target.value
+            console.log(name)
+        }
+        if(event.target.name == "username")
+        {
+            email = event.target.value
+            console.log(email)
+        }
+        
+        if(event.target.name == "password")
+        {
+            password = event.target.value
+            console.log(password)
+        }
+        
+        if(event.target.name == "confirm_password")
+        {
+            confirmPassword = event.target.value
+            console.log(confirmPassword)
+        }
+       
+        
+    }
+
     return ( <div className="container p-5">
     <div className="row bg-light shadow p-4">
         <div className="offset-1 col-5"></div>
         <div className="col border p-4">
             <h4 className="text-center">Welcome User Create Account</h4>
             <form>
-                <div className="form-group mt-4">
-                    
-                    <input type="text" className="form-control" placeholder="Full Name"/>
+                <div className="form-group mt-4"> 
+                    <input type="text" className="form-control" placeholder="Full Name" name="name" onChange={handleChange}/>
+                </div>
+                <div className="form-group mt-4"> 
+                    <input type="text" className="form-control" placeholder="Username/email"name="username" onChange={handleChange}/>
                 </div>
                 <div className="form-group mt-4">
-                    
-                    <input type="text" className="form-control" placeholder="Username/email"/>
+                    <input type="password" className="form-control" placeholder="Password" name="password" onChange={handleChange}/>
                 </div>
                 <div className="form-group mt-4">
-                   
-                    <input type="password" className="form-control" placeholder="Password"/>
+                    <input type="password" className="form-control" placeholder="Confirm Password" name="confirm_password" onChange={handleChange}/>
                 </div>
                 <div className="form-group mt-4">
-                   
-                    <input type="password" className="form-control" placeholder="Confirm Password"/>
-                </div>
-                <div className="form-group mt-4">
-                    
                     <button className="btn btn-dark" type="submit">Signup</button>
                 </div>
             </form>
