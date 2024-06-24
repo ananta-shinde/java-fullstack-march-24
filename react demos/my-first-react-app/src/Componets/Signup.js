449ab89c8081fa655fcf4b202cf786f913c6a312
aShinde <ananta.shinde@honeywellcom>
2024-06-24 13:23:46 +0530

    24/06/24

--- react demos/my-first-react-app/src/Componets/Signup.js
+++ react demos/my-first-react-app/src/Componets/Signup.js
@@ -1,35 +1,68 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Signup = () => {
-    var name = "";
-    var email = ""
-    var password ="";
-    var confirmPassword ="";
+    // var name = "";
+    // var email = ""
+    // var password ="";
+    // var confirmPassword ="";
+
+    const [name,setName] = useState("");
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    const [confirmPassword,setConfirmPassword] = useState("");
+
+    // var error = ""
+    const [error, setError] = useState("")
+
+    function handleSubmit(event)
+    {
+        event.preventDefault()
+        if(name == "" || email==""|| password=="" || confirmPassword=="" )
+        {
+            setError("All fields are compulsary...")
+        }
+        else if(password != confirmPassword){
+            setError("Password Mismatch")
+        }
+        else{
+            setError("")
+            fetch("http://localhost:5000/users",{
+                method:"post",
+                headers:{"Content-Type":"application/json"},
+                body: JSON.stringify({
+                    name,email,password
+                })
+            }).then(res=>res.json())
+            .then(data=>{
+                console.log(data)
+            })
+        }
+        console.log("form submitted")
+    }
     
     function handleChange(event){
-        if(event.target.name == "name"){
-            name = event.target.value
+        if(event.target.name == "name"){ 
+            setName(event.target.value)
             console.log(name)
         }
         if(event.target.name == "username")
         {
-            email = event.target.value
+            setEmail(event.target.value)
             console.log(email)
         }
         
         if(event.target.name == "password")
         {
-            password = event.target.value
+            setPassword(event.target.value)
             console.log(password)
         }
         
         if(event.target.name == "confirm_password")
         {
-            confirmPassword = event.target.value
+            setConfirmPassword(event.target.value)
             console.log(confirmPassword)
-        }
-       
-        
+        } 
     }
 
     return ( <div className="container p-5">
@@ -37,7 +70,8 @@ const Signup = () => {
         <div className="offset-1 col-5"></div>
         <div className="col border p-4">
             <h4 className="text-center">Welcome User Create Account</h4>
-            <form>
+            <form onSubmit={handleSubmit}>
+                {error != "" && <div className="alert alert-danger">{error}</div>}
                 <div className="form-group mt-4"> 
                     <input type="text" className="form-control" placeholder="Full Name" name="name" onChange={handleChange}/>
                 </div>
