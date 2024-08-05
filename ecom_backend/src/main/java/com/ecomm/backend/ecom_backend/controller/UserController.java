591ecfac1bcfa0aa7593a78e93352160efebc789
aShinde <ananta.shinde@honeywellcom>
2024-08-05 13:53:50 +0530

    05/08/24

--- ecom_backend/src/main/java/com/ecomm/backend/ecom_backend/controller/UserController.java
+++ ecom_backend/src/main/java/com/ecomm/backend/ecom_backend/controller/UserController.java
@@ -1,16 +1,22 @@
 package com.ecomm.backend.ecom_backend.controller;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecomm.backend.ecom_backend.model.User;
 import com.ecomm.backend.ecom_backend.repository.UserRepository;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 
 
@@ -26,16 +32,36 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public User createUser(){
-        User u = new User();
-        u.setName("Admin");
-        u.setEmail("admin@demo.com");
-        u.setPassword("admin123");
+    public User createUser(@RequestBody User u){
         u= repository.save(u);
         return u;
     }
 
-    
-    
+    @PutMapping("users/{id}")
+    public User updateUserById(@RequestBody User u,@PathVariable("id") int uid){
+       User existing_user = repository.findById(uid).get();
+       if(u.getName() != null){
+            existing_user.setName(u.getName());
+       }
+       if(u.getContact() != null){
+            existing_user.setContact(u.getContact());
+       }
+       if(u.getPassword() != null){
+            existing_user.setPassword(u.getPassword());
+       }
+       if(u.getAlternate_contact() != null){
+            existing_user.setAlternate_contact(u.getAlternate_contact());
+       }
+      
+        return repository.save(existing_user);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public User deleteUserById(@PathVariable("id") int uid)
+    {
+        User existing_user = repository.findById(uid).get();
+        existing_user.setIs_Active(false);
+        return repository.save(existing_user);
+    }
 
 }
--- ecom_backend/src/main/java/com/ecomm/backend/ecom_backend/model/User.java
+++ ecom_backend/src/main/java/com/ecomm/backend/ecom_backend/model/User.java
@@ -1,5 +1,7 @@
 package com.ecomm.backend.ecom_backend.model;
 
+import java.sql.Date;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
@@ -10,11 +12,63 @@ public class User {
     private String name;
     private String email;
     private String password;
-    public int getId() {
-        return id;
+    private String contact;
+    private String alternate_contact;
+    private int address_id;
+    private String profile_picture;
+    private Boolean is_Active;
+    private Date date_created;
+    private Date date_updated;
+
+
+    public String getContact() {
+        return contact;
     }
     public void setId(int id) {
         this.id = id;
+    }
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+    public String getAlternate_contact() {
+        return alternate_contact;
+    }
+    public void setAlternate_contact(String alternate_contact) {
+        this.alternate_contact = alternate_contact;
+    }
+    public int getAddress_id() {
+        return address_id;
+    }
+    public void setAddress_id(int address_id) {
+        this.address_id = address_id;
+    }
+    public String getProfile_picture() {
+        return profile_picture;
+    }
+    public void setProfile_picture(String profile_picture) {
+        this.profile_picture = profile_picture;
+    }
+    public Boolean getIs_Active() {
+        return is_Active;
+    }
+    public void setIs_Active(Boolean is_Active) {
+        this.is_Active = is_Active;
+    }
+    public Date getDate_created() {
+        return date_created;
+    }
+    public void setDate_created(Date date_created) {
+        this.date_created = date_created;
+    }
+    public Date getDate_updated() {
+        return date_updated;
+    }
+    public void setDate_updated(Date date_updated) {
+        this.date_updated = date_updated;
+    }
+    
+    public int getId() {
+        return id;
     }
     public String getName() {
         return name;
